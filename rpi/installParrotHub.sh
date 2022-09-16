02e0240552a4da2f69cdc6f367e17e89f356c07e
psittaciform <90155287+psittaciform@users.noreply.github.com>
2022-09-16 11:05:02 -0500

    Update installParrotHub.sh

--- rpi/installParrotHub.sh
+++ rpi/installParrotHub.sh
@@ -19,7 +19,7 @@ sudo chown parrot-hub.parrot-hub /opt/parrot-hub
 
 # Download latest version of parrot hub
 cd /tmp
-parrotHubZipLocation=`curl -sL https://github.com/parrot-ha/hub/releases/latest | grep -o "/parrot.*parrot-hub-.*\.zip"`
+parrotHubZipLocation=`curl -sL https://api.github.com/repos/parrot-ha/hub/releases/latest | grep -o "/parrot.*parrot-hub-.*\.zip"`
 curl -L -O --remote-name https://github.com/${parrotHubZipLocation}
 parrotHubZipName=${parrotHubZipLocation##/*/}
 sudo unzip $parrotHubZipName -d /opt/parrot-hub/
